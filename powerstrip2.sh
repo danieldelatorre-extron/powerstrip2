4cab57be82cfa6bed6107ff835cb73983bbd6c16
Daniel de la Torre <ddelatorre@extron.com>
2026-01-28 18:28:52 -0800

    initial commit

---
+++ powerstrip2.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+# Script to control an PDU4H or PDU8L.
+
+username="${PDU_USERNAME:-admin}"
+password="${PDU_PASSWORD:-extron}"
+type="ac"
+
+ip=""
+port=""
+state=""
+
+usage()
+{
+    echo "Usage: powestrip2 [options] -i ip -p port state"
+    echo "Arguments:"
+    echo "    -i ip         IP address or hostname."
+    echo "    -p port       AC or DC power port number."
+    echo "    state         Use 0 for off and 1 for on."
+    echo "Options:"
+    echo "    -u username   Username, default is '${username}'."
+    echo "    -P password   Password, default is '${password}'."
+    echo "    -t type       Power type, default is '${type}'."
+    exit 1
+}
+
+while getopts "i:p:u:p:hP:t:" opt
+do
+    case $opt in
+        i)
+            ip=$OPTARG
+            ;;
+        p)
+            port=$OPTARG
+            ;;
+        u)
+            username=$OPTARG
+            ;;
+        P)
+            password=$OPTARG
+            ;;
+        t)
+            type=$OPTARG
+            ;;
+        h|*)
+            usage
+            ;;
+    esac
+done
+
+shift $((OPTIND-1))
+
+state="$1"
+
+if [ -z "$ip" ]
+then
+    echo "Error: IP address is required."
+    usage
+elif [ -z "$port" ]
+then
+    echo "Error: Port number is required."
+    usage
+elif [ -z "$state" ]
+then
+    echo "Error: State (0 or 1) is required."
+    usage
+else
+    cmd="w${port}*${state}pc\r"
+
+    type_lower="${type,,}"
+    if [ "$type_lower" == "dc" ]
+    then
+        cmd="wp${port}*${state}dcpp\r"
+    fi
+
+    timeout 3s bash -c "printf '$cmd' | sshpass -p ${password} ssh -tt -p 22023 ${username}@${ip}"
+fi
