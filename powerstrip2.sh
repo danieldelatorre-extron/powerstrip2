485149341d6092d472c0d73124a57159ee38575e
Daniel de la Torre <ddelatorre@extron.com>
2026-02-10 13:40:15 -0800

    add 'toggle' as an option for the state argument

--- powerstrip2.sh
+++ powerstrip2.sh
@@ -15,7 +15,7 @@ usage()
     echo "Arguments:"
     echo "    -i ip         IP address or hostname."
     echo "    -p port       AC or DC power port number."
-    echo "    state         Use 0 for off and 1 for on."
+    echo "    state         Valid values are: 0, 1, and toggle."
     echo "Options:"
     echo "    -u username   Username, default is '${username}'."
     echo "    -P password   Password, default is '${password}'."
@@ -57,6 +57,26 @@ shift $((OPTIND-1))
 
 state="$1"
 
+set_power()
+{
+    local val=$1
+    cmd="w${port}*${val}pc\r"
+
+    type_lower="${type,,}"
+    if [ "$type_lower" == "dc" ]
+    then
+        cmd="wp${port}*${val}dcpp\r"
+    fi
+
+    printf '%b' "$cmd" | timeout 3s \
+        sshpass -p "$password" ssh -tt -p 22023 \
+            -o ConnectionAttempts=1 \
+            -o ConnectTimeout=1 \
+            -o StrictHostKeyChecking=no \
+            -o UserKnownHostsFile=/dev/null \
+            "$username@$ip"
+}
+
 if [ -z "$ip" ]
 then
     echo "Error: IP address is required."
@@ -70,26 +90,25 @@ then
     echo "Error: State (0 or 1) is required."
     usage
 else
-    cmd="w${port}*${state}pc\r"
-
-    type_lower="${type,,}"
-    if [ "$type_lower" == "dc" ]
-    then
-        cmd="wp${port}*${state}dcpp\r"
-    fi
-
-    ping "$ip" -c 1 -W 1 > /dev/null 2>&1
-    if [ $? -ne 0 ]
+    if ! ping "$ip" -c 1 -W 1 > /dev/null 2>&1
     then
         echo "Error: Host $ip is unreachable."
         exit 1
     fi
 
-    printf '%b' "$cmd" | timeout 3s \
-        sshpass -p "$password" ssh -tt -p 22023 \
-            -o ConnectionAttempts=1 \
-            -o ConnectTimeout=1 \
-            -o StrictHostKeyChecking=no \
-            -o UserKnownHostsFile=/dev/null \
-            "$username@$ip"
+    case "${state:0:1}" in
+        0|1)
+            set_power "$state"
+            ;;
+        t|T)
+            set_power 0
+            set_power 1
+            ;;
+        *)
+            echo "invalid state"
+            usage
+            ;;
+    esac
+    
+    exit 0
 fi
